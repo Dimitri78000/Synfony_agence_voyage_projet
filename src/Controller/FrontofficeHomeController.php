5f61ee34c742183d76075f6dac79f97227c4a890
Etienne Wan <etiennewan@minet.net>
2018-10-23 12:51:15 +0200

    Like manager

--- src/Controller/FrontofficeHomeController.php
+++ src/Controller/FrontofficeHomeController.php
@@ -16,10 +16,17 @@ class FrontofficeHomeController extends AbstractController
         // entityManager
         $em = $this->getDoctrine()->getManager();
 
+        $likes = $this->get('session')->get('likes');
+        if ($likes === null ){
+            $likes = [];
+            $this->get('session')->set('likes', $likes);
+        }
+
         $prog_circuits = $em->getRepository(ProgrammationCircuit::class)->findAll();
 
         return $this->render('front/home.html.twig', [
-            'prog_circuits' => $prog_circuits
+            'prog_circuits' => $prog_circuits,
+            'likes' => $likes
         ]);
     }
 
@@ -38,5 +45,25 @@ class FrontofficeHomeController extends AbstractController
         return $this->render('front/circuit_show.html.twig', [
             'prog_circuit' => $prog_circuit
         ]);
+    }
+
+    /**
+     * Registers a like and redirects to /home.
+     *
+     * @Route("/likes/{id}", name="likes")
+     */
+    public function likes($id)
+    {
+        $likes = $this->get('session')->get('likes');
+
+        if(!in_array($id, $likes)) {
+            $likes[] = $id;
+        } else {
+            $likes = array_diff($likes, array($id));
+        }
+
+        $this->get('session')->set('likes', $likes);
+
+        return $this->redirect($this->generateUrl('frontoffice_home'));
     }
 }
